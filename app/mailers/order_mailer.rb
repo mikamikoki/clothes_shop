11f95af63cf4d02a517f5328c76c23149376b212
mikamikoki <angjisanshang@gmail.com>
2022-02-15 05:39:43 +0000

    メール本文変更

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,13 +2,13 @@ class OrderMailer < ApplicationMailer
 
   def order_mail(email)
   #mail = ENV['MAIL_ADDRESS']
-  #@customer = customer
+  # @customer = customer
   #mail(to: @customer.email, subject: 'メールのタイトルがここに入ります')
 
   mail(
     from: 'system@example.com',
     to:   email,
-    subject: '購入確定通知'
+    subject: 'ご購入確定通知'
   )
   end
 end
